1b61b55c6a7942e8c07d2ffa6c0f60fd5737a99b
Quentin Homareau <quentin.homareau@gmail.com>
2017-12-21 14:15:45 +0100

    Fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var express = require('express')()
+var app = require('express')()
 
 app.get('/', function(req , res) {
-  
-});
+    res.send('Hello World')
+})
 
-app.listen( process.env.PORT || 8080 , () => console.log('App is running')) ;
+app.listen( process.env.PORT || 8080 , () => console.log('App is running')) 
 
