1f66182202cc58009d12586daf927e4ecb583a19
Quentin Homareau <quentin.homareau@gmail.com>
2017-12-29 12:46:58 +0100

    deprecated send

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')()
 
 app.get('/', function(req , res) {
-    res.send('Hello World')
+    res.end('Hello World')
 })
 
 app.listen( process.env.PORT || 8080 , () => console.log('App is running')) 
